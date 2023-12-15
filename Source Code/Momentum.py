dfbd057b5095fc99c04d473393b8d8f74d998f5a
jelhamm <elhamjafari200312@gmail.com>
2023-12-15 22:24:06 +0330

    Implemented momentum optimization algorithm for minimizing the cost function

---
+++ Source Code/Momentum.py
@@ -0,0 +1,39 @@
+import numpy as np 
+import scipy as sc 
+from sympy import diff, lambdify, symbols, N
+
+
+def momentum(cost_function, func):
+    print("f(x) = ", cost_function)
+    funcDerivative = diff(cost_function, x)
+    print("df(x)/dx = ", funcDerivative)
+    initialApproximation = float(input("Enter initial approximation: "))
+    x0 = initialApproximation
+    learningRate = float(input("Enter learning rate: "))
+    momentumConstant = float(input("Enter momentum constant: "))
+    errorTolerance = float(input("Enter error tolerance: "))
+    print("\n---------------------------------------------------------------")
+    print("Starting Momentum")
+    print("    Initial Approximation (x0) = ", initialApproximation)
+    print("                         f(x0) = ", func(initialApproximation))
+    
+    #---------------------------------------------------------------------------------------------------------------------------------
+    iterationCount = 0
+    xk = x0
+    vk_prev = 0
+    while True:
+        iterationCount += 1
+        func_derivative_value = (lambdify(x, funcDerivative, "numpy"))(xk)            # Calculate the derivative value at current xk
+        vk = momentumConstant * vk_prev + learningRate * func_derivative_value        # Update the velocity using momentum formula
+        xk -= vk                                                                      # Update xk using the velocity
+        # Check if the difference between current xk and previous xk is within the error tolerance
+        if abs(N(xk - x0)) < errorTolerance:
+            break
+        x0 = xk
+        vk_prev = vk
+    #---------------------------------------------------------------------------------------------------------------------------------
+    
+    print("Number of Iterations = ", iterationCount)
+    print("    Minima is at = ", xk)
+    print("    Minimum value of Cost Function = ", func(xk))
+ 
