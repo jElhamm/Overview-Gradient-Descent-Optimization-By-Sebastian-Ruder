e4c9f02ed6e34b51e268e3e47a7a1421a94a02fd
jelhamm <elhamjafari200312@gmail.com>
2023-12-16 00:55:48 +0330

    Add Adagrad optimization algorithm implementation with convergence criteria and output display

--- Source Code/Adagrad.py
+++ Source Code/Adagrad.py
@@ -20,3 +20,24 @@ def adagrad(cost_function, f):
     print("        --->  initial x = ", initialApproximation)
     print("        ---> cost_function(initial x) = ", f(initialApproximation))
  
+    #----------------------------------------------------------------------------------------------------------------------------------------------------------
+    iterationCount = 0
+    previous_x = 0.0
+    sumSquared_gradients = 0
+    epsilon = 10.0 ** -8
+    while True:
+        iterationCount += 1
+        previous_x = current_x
+        fk_derivative = (lambdify(x, f_derivative, "numpy"))(current_x)                           # Compute the derivative for the current_x
+        sumSquared_gradients += fk_derivative ** 2                                                # Update the sum of squared gradients
+        adaptive_learning_rate = learningRate / ((sumSquared_gradients + epsilon) ** 0.5)         # Compute the adaptive learning rate
+        current_x -= adaptive_learning_rate * fk_derivative                                       # Update current_x using the adaptive learning rate
+        if abs(N(current_x - previous_x)) < errorTolerance:
+            break
+    #----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    print(" ***Number of Iterations = ", iterationCount)
+    print("        --->  Minima is at = ", current_x)
+    print("        --->  Minimum value of Cost Function = ", f(current_x))
+    print("---------------------------------------------------------------\n")
+  
